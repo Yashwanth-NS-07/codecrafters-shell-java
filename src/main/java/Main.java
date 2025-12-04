c5a7849770cc0418f676d2e6ea01e98e810b4784
Yashwanth-NS-07 <yashns1859@gmail.com>
2025-12-04 23:12:26 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-        // System.out.print("$ ");
+    public static void main(String[] args) {
+         System.out.print("$ ");
     }
 }
