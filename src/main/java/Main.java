32ca0888ac87cc6e2e0535d26c126b10ee771571
Yashwanth-NS-07 <yashns1859@gmail.com>
2025-12-04 23:46:58 +0530

    invalid added infinite with exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,9 @@ public class Main {
     private static boolean isRunning = true;
     private static Scanner scanner = null;
 
+    private static final String exit = "exit";
+    private static final String echo = "echo";
+
     public static void main(String[] args) {
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
@@ -17,9 +20,30 @@ public class Main {
     public static void startShell() {
         while(isRunning) {
             System.out.print("$ ");
-            String input = scanner.nextLine().trim();
-            if(input.equals("exit")) break;
-            System.out.println(input + ": command not found");
+            String[] cmdAndArgs = parse(scanner.nextLine().trim());
+            assert cmdAndArgs.length == 2;
+
+            switch (cmdAndArgs[0]) {
+                case exit -> {
+                    isRunning = false;
+                }
+                case echo -> {
+                    System.out.println(cmdAndArgs[1]);
+                }
+                default -> {
+                    System.out.println(cmdAndArgs[0] + ": command not found");
+                }
+            }
         }
+    }
+
+    private static String[] parse(String input) {
+        String[] cmdAndArgs = new String[2];
+        int programAndArgSeparatorIndex = input.indexOf(" ");
+        if(programAndArgSeparatorIndex == -1) return new String[] {input, ""};
+
+        cmdAndArgs[0] = input.substring(0, programAndArgSeparatorIndex);
+        cmdAndArgs[1] = input.substring(programAndArgSeparatorIndex+1);
+        return cmdAndArgs;
     }
 }
