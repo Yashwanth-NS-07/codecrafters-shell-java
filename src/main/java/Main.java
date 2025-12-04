fceb677a34f3d2bfd8f92baef0a915b0f69e19b9
Yashwanth-NS-07 <yashns1859@gmail.com>
2025-12-04 23:18:52 +0530

    invalid added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-         System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("$ ");
+        String input = sc.nextLine().trim();
+        System.out.println(input + ": command not found");
+        sc.close();
     }
 }
