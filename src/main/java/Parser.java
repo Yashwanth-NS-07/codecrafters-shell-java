3e06a09ba78b8a15f9e90ae90336c904d1197ff3
Yashwanth-NS-07 <yashns1859@gmail.com>
2025-12-13 11:13:27 +0530

    code added for 2>>

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -73,7 +73,7 @@ public class Parser {
                             sb.delete(0, sb.length());
                         }
                     } else if(c == '>') {
-                        if(sb.equals("2")) {
+                        if(sb.isEmpty()) {
                             isErrorAppend = true;
                         }
                     } else if(c == '|') {
